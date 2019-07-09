c6a23315ae54753d5835ff0aa68e3465097e6cfa
gdevkate34 <gdevkate34@gmail.com>
2019-07-09 14:40:07 +0530

    store json format changed

--- resources/store.py
+++ resources/store.py
@@ -31,4 +31,4 @@ class Store(Resource):
 
 class StoreList(Resource):
 	def get(self):
-		return {'stores':[store.json() for store in StoreModel.query.all()]}
+		return {[store.json() for store in StoreModel.query.all()]}
